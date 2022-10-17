53b5e2debddca734a38a44bbc5d3aa4ce5770bcd
mrcaidev <mrcaidev@gmail.com>
2022-10-17 09:12:45 +0800

    feat: add index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,34 @@
-const Home = () => <h1>Hello</h1>;
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+import logo from "public/logo.svg";
+import styles from "src/styles/home.module.css";
+
+const Home = () => (
+  <>
+    <Head>
+      <title>OG - Yuwang Cai</title>
+    </Head>
+    <main className={styles.container}>
+      <Image src={logo} alt="Logo" width="44" height="60" />
+      <h1>og.mrcai.dev</h1>
+      <p>OG Image Generator</p>
+      <p className={styles.example}>
+        Visit&nbsp;
+        <Link href="/api/home">
+          <a className={styles.link}>/api/home</a>
+        </Link>
+        &nbsp;for homepage OG image
+      </p>
+      <p className={styles.example}>
+        Visit&nbsp;
+        <Link href="/api/blog?title=Hello%20world">
+          <a className={styles.link}>/api/blog?title=Hello%20world</a>
+        </Link>
+        &nbsp;for blog post OG image
+      </p>
+    </main>
+  </>
+);
 
 export default Home;
